9af4dc471c1f4a4794faabd9a985cac3c63be634
ImAgainBack <artsmogunov@gmail.com>
2023-03-23 19:53:36 +0100

    Update functions_2.py

--- functions_2.py
+++ functions_2.py
@@ -1,7 +1,7 @@
-from functions_1 import *
+from functions_1 import * #getting functions from functions_1
 
-def determinized():
-    def find_duplicates(lst, indices):
+def determinized(): #function to check if the automata is determinized 
+    def find_duplicates(lst, indices): #by so we will find duplicates in the 2d array, check comments in functions_1
         seen = set()
         duplicates = set()
         for i, sublist in enumerate(lst):
@@ -11,14 +11,14 @@ def determinized():
             seen.add(key)
         return duplicates
 
-    duplicates = find_duplicates(data_6th_line, [0,1])
+    duplicates = find_duplicates(data_6th_line, [0,1]) #[0,1] are elements in the 2d array, so we will check in the loop below if there is a match
         
-    if duplicates==set():
-        return True
+    if duplicates==set(): #if no duplicates
+        return True #this automata is determinized
     else:
         return False
 
-def complete(list):
+def complete(list): 
     if len(third_line())==1:
         
         return True
