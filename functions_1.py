93b86e6546edb0279c72e95612d337f620b8be34
ImAgainBack <artsmogunov@gmail.com>
2023-03-23 19:51:01 +0100

    Update functions_1.py

--- functions_1.py
+++ functions_1.py
@@ -1,14 +1,14 @@
-def lines(line):
+def lines(line): #a function that gents 1 line and splits values in a list
     data = line.split()
     return data
 
-def remaining_lines(line):
+def remaining_lines(line): #function that gets used in the 6th line and so on
     data = line.split()
-    data_6th_line.append(data)
+    data_6th_line.append(data) #for that we create a 2d array
     return data
 
-data_6th_line=[]
-with open('text_file.txt') as file:
+data_6th_line=[] #creation of the 2d array
+with open('text_file.txt') as file: #opening the txt file containing automata
     for i, line in enumerate(file):
         if i == 0:
             first_line=lines(line)
@@ -24,17 +24,16 @@ with open('text_file.txt') as file:
             remaining_lines(line)
 
 
-def standart():
-    state=True
-    print(third_line)
-    if len(third_line)==1:
-        initial_state=third_line[0]
+def standart(): #checking if the automata is standart
+    state=True #bool value check
+    if len(third_line)==1: #cheking if there is multiple initial states, if so will go to line 38 and print False, meaning not standart
+        initial_state=third_line[0] #collecting the int value of the 4rd line, which is the initial state
 
-        for i in range(len(data_6th_line)):
+        for i in range(len(data_6th_line)): #loop to lopp through all the elements in the 2d array
     
-            if initial_state==data_6th_line[i][2]:
+            if initial_state==data_6th_line[i][2]:#loop to check if there is a match between the 3rd element of the 2d array 
 
-                state=False
+                state=False #if there is a match between the last element of the 2d array and the initial state, not standart automata
     else:
-        return False
+        return False 
     return state
